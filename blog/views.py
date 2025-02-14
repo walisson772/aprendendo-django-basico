0e86195afff19ba37fa58fd45109c13d6ed6c51a
Walisson Santos <walissonsantost008@gmail.com>
2025-02-14 16:18:03 -0300

    For loop
    Usando for loop para adicionar novos codigos html no site

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from blog.data import posts
 
 
 # Create your views here.
@@ -6,6 +7,7 @@ def blog(request):
     context = {
             'text': 'Estou no blog',
             'title': 'Blog - ',
+            'posts': posts
         }
     return render(
         request,
